d910083dd50a9c59aadfd4ee26240461b66c4937
Nishal <nishaldevadiga2003@gmail.com>
2023-02-06 18:13:55 +0530

    3rd commit

--- app/src/main/java/com/example/basicactivityswitching01/Adapter/ImagesAdapter.kt
+++ app/src/main/java/com/example/basicactivityswitching01/Adapter/ImagesAdapter.kt
@@ -3,13 +3,14 @@ package com.example.basicactivityswitching01.Adapter
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.basicactivityswitching01.Model.Images
 import com.example.basicactivityswitching01.R
 
 class ImagesAdapter(val context: Context, val images: List<Images>): RecyclerView.Adapter<ImagesAdapter.Holder>() {
     inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val imageImage = itemView.findViewById<R.id.>()
+        val imageImage = itemView.findViewById<ImageView>()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
