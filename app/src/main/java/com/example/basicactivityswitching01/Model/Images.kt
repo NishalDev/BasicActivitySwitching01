21895edb56e949c1e693a5088ad2cf907e5ab051
Nishal <nishaldevadiga2003@gmail.com>
2023-02-07 18:51:43 +0530

    8th commit

--- app/src/main/java/com/example/basicactivityswitching01/Model/Images.kt
+++ app/src/main/java/com/example/basicactivityswitching01/Model/Images.kt
@@ -1,4 +1,31 @@
 package com.example.basicactivityswitching01.Model
 
-class Images(val images: String,val title: String) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Images(val images: String?,val title: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(images)
+        parcel.writeString(title)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Images> {
+        override fun createFromParcel(parcel: Parcel): Images {
+            return Images(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Images?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
