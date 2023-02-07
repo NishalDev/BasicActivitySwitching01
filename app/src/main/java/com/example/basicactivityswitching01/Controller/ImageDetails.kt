30824a34a3987704d156533831798f2156f982f5
Nishal <nishaldevadiga2003@gmail.com>
2023-02-07 20:29:28 +0530

    11th commit

--- app/src/main/java/com/example/basicactivityswitching01/Controller/ImageDetails.kt
+++ app/src/main/java/com/example/basicactivityswitching01/Controller/ImageDetails.kt
@@ -2,12 +2,14 @@ package com.example.basicactivityswitching01.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.basicactivityswitching01.Adapter.ImageDetailsAdapter
 import com.example.basicactivityswitching01.Adapter.ImagesAdapter
 import com.example.basicactivityswitching01.Model.Images
 import com.example.basicactivityswitching01.R
 import com.example.basicactivityswitching01.Services.Data
 import com.example.basicactivityswitching01.Utilities.EXTRA_IMAGE
+import kotlinx.android.synthetic.main.activity_image_details.*
 
 class ImageDetails : AppCompatActivity() {
     lateinit var adapter: ImageDetailsAdapter
@@ -19,6 +21,8 @@ class ImageDetails : AppCompatActivity() {
 
         adapter = ImageDetailsAdapter(this,Data.getImageDetail(imageType))
 
-
+        val layoutManager = GridLayoutManager(this,1)
+        imageDetailsRecycler.layoutManager = layoutManager
+        imageDetailsRecycler.adapter = adapter
     }
 }
