e38406cd48cfb5f4b71efa37faf6d8e73388a043
Nishal <nishaldevadiga2003@gmail.com>
2023-02-10 22:53:14 +0530

    reformat

--- app/src/main/java/com/example/basicactivityswitching01/Adapter/CategoryRecyclerAdapter.kt
+++ app/src/main/java/com/example/basicactivityswitching01/Adapter/CategoryRecyclerAdapter.kt
@@ -10,13 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.basicactivityswitching01.Model.Categories
 import com.example.basicactivityswitching01.R
 
-class CategoryRecyclerAdapter(val context: Context, val categories: List<Categories>, val itemClick: (Categories)-> Unit) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder>() {
-    inner class Holder(itemView: View, val itemClick: (Categories) -> Unit) :RecyclerView.ViewHolder(itemView){
+class CategoryRecyclerAdapter(
+    val context: Context,
+    val categories: List<Categories>,
+    val itemClick: (Categories) -> Unit
+) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder>() {
+    inner class Holder(itemView: View, val itemClick: (Categories) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
         val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
         val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
 
-        fun bindCategory(categories: Categories,context: Context){
-            val resourceId = context.resources.getIdentifier(categories.image,"drawable",context.packageName)
+        fun bindCategory(categories: Categories, context: Context) {
+            val resourceId =
+                context.resources.getIdentifier(categories.image, "drawable", context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text = categories.title
 
@@ -25,15 +31,16 @@ class CategoryRecyclerAdapter(val context: Context, val categories: List<Categor
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_list,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.category_list, parent, false)
         return Holder(view, itemClick)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bindCategory(categories[position],context)
+        holder.bindCategory(categories[position], context)
     }
 
     override fun getItemCount(): Int {
-       return categories.count()
+        return categories.count()
     }
 }
